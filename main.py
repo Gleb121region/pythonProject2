2c7b107b22da3c02a3b7d6066501b4db4880ff7a
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2022-12-23 01:11:45 +0300

    add

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from amzqr import amzqr
 
 def main():
     msg = 'https://github.com'
-    amzqr.run(msg, version= 4 , level='L',
-              picture='шрек.gif', colorized=True)
+    amzqr.run(msg,
+              version= 4 ,
+              level='L',
+              picture='шрек.gif',
+              colorized=True)
 
 if __name__ == '__main__':
     main()
