cd514a1df0d9ab64a1a14ee473614eb6c81881ec
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2022-12-23 01:08:43 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from amzqr import amzqr
+
+
+def main():
+    msg = 'https://github.com'
+    amzqr.run(msg, version= 4 , level='L',
+              picture='шрек.gif', colorized=True)
+
+if __name__ == '__main__':
+    main()
